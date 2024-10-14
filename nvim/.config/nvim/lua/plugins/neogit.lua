6bd2b232be7aa92c7f6891ebdb4a2b91971bb00a
Chee Wai Ooi <cheewaio@hotmail.com>
2024-10-14 10:37:03 -0400

    add gitsigns

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -6,6 +6,14 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   config = function()
-    require("neogit").setup({})
+    require("neogit").setup({
+      integration = {
+        diffview = true
+      },
+      kind = "floating",
+    })
+    vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Neogit" })
+    vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = "Open Neogit in Diff View" })
+    vim.keymap.set("n", "<leader>gl", ":Neogit log<CR>", { desc = "Open Neogit log" })
   end
 }
